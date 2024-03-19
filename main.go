4a0ee9f66401c7e0eff77b44b75d3ffa7538fd75
fullgukbap <2hanjunbum6@gmail.com>
2024-03-19 10:49:08 +0900

    feat: rootHandler 구현 완료했습니다.

--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"math/rand"
+	"net/http"
+)
 
+func main() {
+	r := http.NewServeMux()
+	r.HandleFunc("GET /", RootHandler)
+
+	log.Fatal(http.ListenAndServe(":9000", r))
+}
+
+func RootHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Random number is : %v", GetRandomNumber())
+}
+
+func GetRandomNumber() int {
+	return rand.Intn(1000)
 }
